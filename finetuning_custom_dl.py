73b076fb2a0c3e04821cdaffea4ebb0b5d153d7f
Simone Mattioli <simone.mattioli6@studio.unibo.it>
2021-08-13 15:30:21 +0200

    update

--- finetuning_custom_dl.py
+++ finetuning_custom_dl.py
@@ -44,6 +44,7 @@ from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Union
 from transformers import Wav2Vec2ForCTC
 
+
 @dataclass
 class DataCollatorCTCWithPadding:
     """
@@ -77,8 +78,37 @@ class DataCollatorCTCWithPadding:
     pad_to_multiple_of: Optional[int] = None
     pad_to_multiple_of_labels: Optional[int] = None
 
+    def __init__(self, processor, padding=True, apply_gaussian_noise_with_p=0.5, apply_gain_with_p=0.5, apply_pitch_shift_with_p=0.5, 
+                 apply_time_stretch_with_p=0.5, sample_rate=16_000):
+        self.processor = processor
+        self.padding = padding
+        self.apply_gaussian_noise_with_p = apply_gaussian_noise_with_p
+        self.apply_gain_with_p = apply_gain_with_p
+        self.apply_pitch_shift_with_p = apply_pitch_shift_with_p
+        self.apply_time_stretch_with_p = apply_time_stretch_with_p
+        self.sample_rate = sample_rate
+
+        self.augmentator = None
+        if self.apply_gaussian_noise_with_p + self.apply_gain_with_p + self.apply_pitch_shift_with_p + self.apply_time_stretch_with_p > 0:
+            self.augmentator = Compose([
+                TimeStretch(min_rate=0.8, max_rate=1.2, leave_length_unchanged=False, p=self.apply_time_stretch_with_p),
+                PitchShift(min_semitones=-1, max_semitones=1, p=self.apply_pitch_shift_with_p),
+                Gain(min_gain_in_db=-1, max_gain_in_db=1, p=self.apply_gain_with_p),
+                AddGaussianNoise(min_amplitude=0.0001, max_amplitude=0.001, p=self.apply_gaussian_noise_with_p),
+            ])
+
+    def _apply_augmentation(self, input_values: List[float]):
+        """apply some audio augmentations in the given input_values"""
+        if self.augmentator is not None:
+            return self.augmentator(samples=np.array(input_values), sample_rate=self.sample_rate).tolist()
+        else:
+            return input_values
+
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        input_features = [{"input_values": feature["input_values"]} for feature in features]
+        # split inputs and labels since they have to be of different lenghts and need
+        # different padding methods
+
+        input_features = [{"input_values": self._apply_augmentation(feature["input_values"])} for feature in features]
         label_features = [{"input_ids": feature["labels"]} for feature in features]
 
         batch = self.processor.pad(
@@ -97,11 +127,145 @@ class DataCollatorCTCWithPadding:
                 return_tensors="pt",
             )
 
+        # replace padding with -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
 
         batch["labels"] = labels
 
         return batch
+
+
+class CTCTrainer(Trainer):
+
+    def __init__(self, model_output_dir, length_field_name="length", upload_model_to_wandb_each_step=None, lr_warmup_ratio=0.1, 
+                lr_constant_ratio=0.4, sampling_rate=16_000, **kwargs):
+        super().__init__(**kwargs)
+        self.model_output_dir = model_output_dir
+        self.length_field_name = length_field_name
+        self.upload_model_to_wandb_each_step = upload_model_to_wandb_each_step
+        self.lr_warmup_ratio = lr_warmup_ratio
+        self.lr_constant_ratio = lr_constant_ratio
+        self.sampling_rate = sampling_rate
+
+    def _get_train_sampler(self) -> Optional[torch.utils.data.sampler.Sampler]:
+        if isinstance(self.train_dataset, torch.utils.data.IterableDataset) or not isinstance(
+            self.train_dataset, collections.abc.Sized
+        ):
+            return None
+
+        # Build the sampler.
+        if self.args.group_by_length:
+            lengths = self.train_dataset[self.length_field_name] if self.length_field_name is not None else None
+            model_input_name = self.tokenizer.model_input_names[0] if self.tokenizer is not None else None
+            if self.args.world_size <= 1:
+                return LengthGroupedSampler(
+                    self.train_dataset, self.args.train_batch_size, lengths=lengths, model_input_name=model_input_name
+                )
+            else:
+                return DistributedLengthGroupedSampler(
+                    self.train_dataset,
+                    self.args.train_batch_size,
+                    num_replicas=self.args.world_size,
+                    rank=self.args.process_index,
+                    lengths=lengths,
+                    model_input_name=model_input_name,
+                )
+
+        else:
+            return super()._get_train_sampler()
+
+    def create_scheduler(self, num_training_steps: int):
+        """
+        Setup the scheduler. The optimizer of the trainer must have been set up before this method is called.
+        This method was built based on https://arxiv.org/pdf/2006.13979 :
+            "The learning rate schedule has three phases: warm up for the first 10% of updates, 
+             keep constant for 40% and then linearly decay for the remainder"
+        
+        Args:
+            num_training_steps (int): The number of training steps to do.
+        """
+        def lr_lambda(current_step):
+            warmup_steps = int(num_training_steps * self.lr_warmup_ratio)
+            constant_steps = int(num_training_steps * self.lr_constant_ratio)
+            if current_step < warmup_steps:
+                return float(current_step) / float(max(1, warmup_steps))
+            elif (self.lr_warmup_ratio + self.lr_constant_ratio) == 1.0 or current_step < (warmup_steps + constant_steps):
+                return 1
+            else: 
+                return max(
+                    0.0, float(num_training_steps - current_step) / float(max(1, num_training_steps - (warmup_steps + constant_steps)))
+                )
+        
+        self.lr_scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda)
+
+    def _apply_some_audio_transformations(self, inputs):
+        """Perform some audio transformations"""
+        
+        # adding an extra dimmention for the channels as our data is mono audio and
+        # the expected shape of input for torch_audiomentations is (batch_size, num_channels, num_samples)
+        transformed_inputs = inputs["input_values"].unsqueeze(1)
+
+        transformed_inputs = self.augmentator(transformed_inputs, sample_rate=self.sampling_rate)
+           
+        # returning the inputs to the original shape
+        transformed_inputs = torch.squeeze(transformed_inputs, 1)
+        
+        inputs["input_values"] = transformed_inputs
+
+        return inputs
+
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+        Subclass and override to inject custom behavior.
+        Args:
+            model (:obj:`nn.Module`):
+                The model to train.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+        Return:
+            :obj:`torch.Tensor`: The tensor with training loss on this batch.
+        """
+
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+
+        if self.use_amp:
+            with autocast():
+                loss = self.compute_loss(model, inputs)
+        else:
+            loss = self.compute_loss(model, inputs)
+
+        if self.args.n_gpu > 1:
+            if model.module.config.ctc_loss_reduction == "mean":
+                loss = loss.mean()
+            elif model.module.config.ctc_loss_reduction == "sum":
+                loss = loss.sum() / (inputs["labels"] >= 0).sum()
+            else:
+                raise ValueError(f"{model.config.ctc_loss_reduction} is not valid. Choose one of ['mean', 'sum']")
+
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        if self.use_amp:
+            self.scaler.scale(loss).backward()
+        elif self.use_apex:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        elif self.deepspeed:
+            self.deepspeed.backward(loss)
+        else:
+            loss.backward()
+
+        if self.upload_model_to_wandb_each_step is not None and self.state.global_step > 0 \
+            and self.state.global_step % self.upload_model_to_wandb_each_step == 0:
+            upload_model_to_wandb(self.model_output_dir, name=f"{wandb.run.name}_{self.state.global_step}", metadata={"loss": float(loss)})
+
+        return loss.detach()
+
+
 
 def show_random_elements(dataset, num_examples=1):
     assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
@@ -116,15 +280,10 @@ def show_random_elements(dataset, num_examples=1):
     print(df)
 
 
-
-
-
-
-def extract_all_chars(batch):
-    all_text = " ".join(batch)
-    vocab = list(set(all_text))
-    return {"vocab": [vocab], "all_text": [all_text]}
-
+# def extract_all_chars(batch):
+#     all_text = " ".join(batch)
+#     vocab = list(set(all_text))
+#     return {"vocab": [vocab], "all_text": [all_text]}
 
 def compute_metrics(pred):
     pred_logits = pred.predictions
@@ -141,31 +300,28 @@ def compute_metrics(pred):
     return {"wer": wer}
 
 
+
+
 print("#### Loading dataset")
 
-print("####Commonvoice")
-common_voice_train = load_dataset("common_voice", "it", split=f'train')
-common_voice_val = load_dataset("common_voice", "it", split=f'validation')
+# print("####Commonvoice")
+# common_voice_train = load_dataset("common_voice", "it", split=f'train')
+# common_voice_val = load_dataset("common_voice", "it", split=f'validation')
 
-common_voice_train = common_voice_train.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
-common_voice_val = common_voice_val.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
+# common_voice_train = common_voice_train.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
+# common_voice_val = common_voice_val.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "segment", "up_votes"])
 
 print("####MLLS")
 
 main_dir = "dataset/MultilingualLibriSpeech/mls_italian_opus"
-
-
 
 train_dir = join(main_dir, 'train')
 #val_dir = join(main_dir, 'test')
 
 file_transcripts = 'transcripts.txt'
 
-
-list_txt_train = list()
-list_txt_val = list()
-
-
+list_txt_train = []
+list_txt_val = []
 
 CHARS_TO_IGNORE = [",", "?", "¿", ".", "!", "¡", ";", ";", ":", '""', "%", '"', "?", "?", "·", "?", "~", "?",
                    "?", "?", "?", "?", "«", "»", "„", "“", "”", "?", "?", "‘", "’", "«", "»", "(", ")", "[", "]",
@@ -184,12 +340,9 @@ def remove_special_characters_comm(batch):
     return batch
 
 def create_hug_dataset(split_directory, split_train):
-
-
-    list_opus = list()
+    
+    list_opus = []
     labels_dict = {}
-    audio_dict = {}
-
     for (dirpath, dirnames, filenames) in walk(split_directory):
         list_opus += [join(dirpath, file) for file in filenames if file.endswith(".opus")]
 
@@ -197,15 +350,13 @@ def create_hug_dataset(split_directory, split_train):
         content = f.read()
         sentences = content.split(sep="\n")
 
-
     for sent in sentences:
         if(sent != ''):
             sent = re.sub(' +', ' ', sent)
             sent = sent.split("\t", maxsplit=1)
             labels_dict[sent[0]] = sent[1]
 
-    for opus in list_opus:
-        audio_dict[opus.split("/")[-1].split(".")[0]] = opus
+    audio_dict = {opus.split("/")[-1].split(".")[0]: opus for opus in list_opus}
 
     print("#### Removing special characters from labels mlls")
 
@@ -224,27 +375,34 @@ def create_hug_dataset(split_directory, split_train):
 
     return Dataset.from_dict(hug_dataset[:n_data_train]), Dataset.from_dict(hug_dataset[n_data_train:])
 
+
+
 hug_dataset_train, hug_dataset_val = create_hug_dataset(train_dir, split_train=80)
 
 
-common_voice_train = common_voice_train.map(remove_special_characters_comm)
-common_voice_val = common_voice_val.map(remove_special_characters_comm)
-print(common_voice_train["sentence"][0])
-print(hug_dataset_train)
-print(hug_dataset_val)
-print(common_voice_train)
-print(common_voice_val)
 
-print("####Merge datasets")
+# common_voice_train = common_voice_train.map(remove_special_characters_comm)
+# common_voice_val = common_voice_val.map(remove_special_characters_comm)
+# print(common_voice_train["sentence"][0])
+# print(hug_dataset_train)
+# print(hug_dataset_val)
+# print(common_voice_train)
+# print(common_voice_val)
 
-merged_dataset_train = concatenate_datasets([hug_dataset_train, common_voice_train])
-merged_dataset_val = concatenate_datasets([hug_dataset_val, common_voice_val])
-print(merged_dataset_train)
-print(merged_dataset_val)
+
+
+# print("####Merge datasets")
+
+# merged_dataset_train = concatenate_datasets([hug_dataset_train, common_voice_train])
+# merged_dataset_val = concatenate_datasets([hug_dataset_val, common_voice_val])
+# print(merged_dataset_train)
+# print(merged_dataset_val)
 #merged_dataset_train = Dataset.from_dict(merged_dataset_train)
 #merged_dataset_val = Dataset.from_dict(merged_dataset_val)
 #print(merged_dataset_train)
 #print(merged_dataset_val)
+
+
 #print("#### Creating vocabolary")
 #all_text = " ".join([v for k, v in labels_dict_train.items()])
 #vocab = list(set(all_text))
@@ -286,23 +444,27 @@ print(merged_dataset_val)
 
 #processor.save_pretrained('./finetuning_fb_ita_comm_mlls/processor')
 
+
+
 processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-italian")
 
 data_collator = DataCollatorCTCWithPadding(
     processor=processor, 
     padding=True,
-    #apply_gaussian_noise_with_p=0.5,
-    #apply_gain_with_p=0.5,
-    #apply_pitch_shift_with_p=0.5,
-    #apply_time_stretch_with_p=0.5,
-    #sample_rate=16_000,
+    apply_gaussian_noise_with_p=0.5,
+    apply_gain_with_p=0.5,
+    apply_pitch_shift_with_p=0.5,
+    apply_time_stretch_with_p=0.5,
+    sample_rate=16_000,
 )
 
 wer_metric = load_metric("wer")
 
+
+
 print("#### Creating model")
 model = Wav2Vec2ForCTC.from_pretrained(
-    "jonatasgrosman/wav2vec2-large-xlsr-53-italian",
+    "facebook/wav2vec2-large-xlsr-53",
     attention_dropout=0.1,
     activation_dropout=0.1,
     hidden_dropout=0.1,
@@ -317,7 +479,6 @@ model = Wav2Vec2ForCTC.from_pretrained(
 )
 
 model.freeze_feature_extractor()
-
 
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
@@ -365,7 +526,7 @@ dataset_val = CustomDataset(dataset = hug_dataset_val, processor = processor)
 
 print("#### Initializing training arguments")
 training_args = TrainingArguments(
-            output_dir='./finetuning_jonata_ita_all/checkpoints',
+            output_dir='./finetuning_fbxlsr53_mlls/checkpoints',
             group_by_length=True,
             do_train=True,
             do_eval=True,
@@ -383,12 +544,14 @@ training_args = TrainingArguments(
             dataloader_num_workers=8,
             warmup_ratio=0.1, #~10% of training
             save_total_limit=1,
-            logging_dir='./finetuning_jonata_ita_all/logs'
        )
 
 print("#### Creating Trainer")
-trainer = Trainer(
+trainer = CTCTrainer(
         model=model,
+        length_field_name="length",
+        lr_warmup_ratio = 0.1,
+        lr_constant_ratio = 0.4,
         data_collator=data_collator,
         args=training_args,
         compute_metrics=compute_metrics,
@@ -404,9 +567,9 @@ try:
     trainer.train()
 
     print("#### Save model")
-    trainer.save_model('./finetuning_jonata_ita_all/final')
+    trainer.save_model('./finetuning_fbxlsr53_mlls/final')
 except Exception as e:
-    with open('./finetuning_jonata_ita_all/error.txt', 'w') as f:
+    with open('./finetuning_fbxlsr53_mlls/error.txt', 'w') as f:
         f.write(str(e))
     print(str(e))
 
